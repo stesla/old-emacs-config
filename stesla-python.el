1e94f6ca18d01ddd7a439a4dfeadb8ee1687c47d
Samuel Tesla <samuel@gogrid.com>
2010-07-14 15:53:31 -0700

    Add ropemacs support.

--- stesla-python.el
+++ stesla-python.el
@@ -1,7 +1,18 @@
+;; download and install Pymacs
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
 (autoload 'pymacs-eval "pymacs" nil t)
 (autoload 'pymacs-exec "pymacs" nil t)
 (autoload 'pymacs-load "pymacs" nil t)
+
+;; easy_install rope
+;; easy_install ropemode
+;; download and install ropemacs
+(defun load-ropemacs ()
+  "Load ropemacs"
+  (interactive)
+  (pymacs-load "ropemacs" "rope-")
+  ;; Automatically save project python buffers before refactorings
+  (setq ropemacs-confirm-saving 'nil))
 
 (provide 'stesla-python)
