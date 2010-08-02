37ff8de9da187cb2e6f860a5610940152c1f1bfb
Samuel Tesla <samuel@gogrid.com>
2010-08-02 13:45:13 -0700

    Added some more keybindings

--- stesla-bindings.el
+++ stesla-bindings.el
@@ -1,5 +1,9 @@
-(global-set-key (kbd "C-c l") 'goto-line)
+(dolist (b `((,(kbd "C-c l") goto-line)
+             (,(kbd "C-c r") compile)))
+  (global-set-key (car b) (cadr b)))
 
-(set-register ?. `(file . ,(concat dotfiles-dir "init.el")))
+(dolist (r `((?. (file . ,(concat dotfiles-dir "init.el")))
+             (?b (file . ,(concat dotfiles-dir "stesla-bindings.el")))))
+  (set-register (car r) (cadr r)))
 
 (provide 'stesla-bindings)
