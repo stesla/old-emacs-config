f51cb5f7775fd1d2fa16f0bd42ee6e9540c45abe
Samuel Tesla <samuel.tesla@gmail.com>
2011-01-18 17:18:16 -0800

    tweaked font settings

--- custom.el
+++ custom.el
@@ -35,6 +35,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "gray4" :foreground "light gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(smerge-refined-change ((t (:background "dark red")))))
 
 ;; Enabled functions
