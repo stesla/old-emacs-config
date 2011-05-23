16d7dfbdafa7248064ccbf46d33f5f5d9d185d76
Samuel Tesla <samuel.tesla@gmail.com>
2011-05-23 17:05:23 -0500

    added back auto-insert of file templates

--- stesla-auto.el
+++ stesla-auto.el
@@ -22,9 +22,7 @@
 (require 'autoinsert)
 (auto-insert-mode)
 
-(setq auto-insert-alist nil)
 (setq auto-insert-directory (concat dotfiles-dir "templates/"))
-(setq auto-insert-query nil)
 
 (define-auto-insert "\.py" "python.py")
 
